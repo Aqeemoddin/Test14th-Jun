8c67d48ac9683871fb14ee6eedc389f761c8beb9
Aqeemoddin <aqeemoddin.quazi@gmail.com>
2022-07-14 16:54:50 +0530

    first commit

---
+++ src/main/java/com/example/PasswordED.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PasswordED {
+
+	public static String encode(String str) {
+		
+		Encoder encoder=Base64.getEncoder();
+		return encoder.encodeToString(str.getBytes());
+	}
+	
+	public static String decode(String name) {
+		Decoder decoder=Base64.getDecoder();
+		byte[] decode=decoder.decode(name);
+		return new String(decode);
+	}
+	
+	public static void main(String[] args) {
+		
+		String name=PasswordED.encode("Aqeemoddin");
+		System.out.println(name);
+
+		String name2=PasswordED.decode(name);
+		System.out.println(name2);
+	}
+
+}
